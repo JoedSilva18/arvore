a1feb22ce949c3dd5506e8a62bfb6ef3c7b14a58
Joed Silva <joedsilva2016@gmail.com>
2020-07-05 20:01:48 -0300

    Alterando retorno criacao usuario

--- src/app/controllers/UserControllers.js
+++ src/app/controllers/UserControllers.js
@@ -4,7 +4,10 @@ class UserController {
   async store(req, res) {
     const user = await User.create(req.body);
 
-    return res.status(200).json(user);
+    if (!user) {
+      return res.status(400).send(false);
+    }
+    return res.status(200).send(true);
   }
 }
 
